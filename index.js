a9ddf4373d1f055915061450db24991a23584664
Azam Shaikh <121885327+AzamCodes@users.noreply.github.com>
2023-08-31 17:11:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// var crsr = document.querySelector("#circle");
+// var main = document.querySelector("#main");
+
+// main.addEventListener("mousemove", function (dets) {
+//   crsr.style.left = dets.x + "px";
+//   crsr.style.top = dets.y + "px";
+// });
